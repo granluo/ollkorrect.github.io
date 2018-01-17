07123323340d1e03e1d8b856e317e2d6359ee4de
unknown <ollkorrect999@gmail.com>
2018-01-17 03:07:57 -0800

    test img size

--- md.php
+++ md.php
@@ -31,30 +31,7 @@
 
 
 <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="index.html#page-top">Zongran's Playground</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="index.html#about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="index.html#services">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="index.html#portfolio">Portfolio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="index.html#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php include "navbar.php"; ?>
 
 <body unresolved>
     <style>
